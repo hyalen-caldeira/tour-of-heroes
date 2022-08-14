f85707917f68ed5e9178ae7834f3cd3db5ae0212
Hyalen Moreira Caldeira <hyalen.moreira@gmail.com>
2022-08-14 13:49:25 -0400

    Get data from a service using Observable and Subject

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,27 +4,44 @@ import { HeroService } from '../service/hero.service';
 import { MessageService } from '../service/message.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes : Hero[] = [];
-  // selectedHero? : Hero;
+    heroes: Hero[] = [];
+    // selectedHero? : Hero;
 
-  constructor(private heroService : HeroService, private messageService : MessageService) { }
+    constructor(private heroService: HeroService, private messageService: MessageService) { }
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
+    getHeroes(): void {
+        this.heroService.getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
+    }
 
-  // onSelect(hero: Hero) {
-  //   this.selectedHero = hero;
-  //   this.messageService.add(`HeroesComponent: Selected hero id = ${hero.id}`);
-  // }
+    add(name: string): void {
+        name = name.trim();
+
+        if (!name) 
+            return;
+
+        this.heroService.addHero({ name } as Hero)
+            .subscribe(hero => {
+                this.heroes.push(hero);
+            });
+    }
+
+    delete(hero: Hero): void {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        this.heroService.deleteHero(hero.id).subscribe();
+    }
+
+    // onSelect(hero: Hero) {
+    //   this.selectedHero = hero;
+    //   this.messageService.add(`HeroesComponent: Selected hero id = ${hero.id}`);
+    // }
 }
