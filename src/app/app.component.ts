600e88dec258cc1799df16be1aa433fdec0418c7
Hyalen Moreira Caldeira <hyalen.moreira@gmail.com>
2022-09-05 16:09:31 -0400

    Removed modules.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { PupComponent } from './pup/pup.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    title = 'Tour of Heroes';
+
+    @ViewChild(PupComponent) pup!: PupComponent;
+    
+    ngAfterViewInit() {
+        console.log(this.pup.whoAmI()); // I am a pup component!
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,43 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppComponent } from './app.component';
-import { ExamplesModule } from './examples/examples.module'
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ExamplesRoutingModule } from './examples-routing.module';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { MessagesComponent } from './messages/messages.component';
+import { InMemoryDataService } from './service/in-memory-data.service';
+import { PupComponent } from './pup/pup.component';
 
 @NgModule({
-    declarations: [ AppComponent ],
-    imports: [ ExamplesModule ],
+    declarations: [
+        AppComponent
+        , DashboardComponent
+        , MessagesComponent
+        , HeroSearchComponent
+        , HeroDetailComponent
+        , HeroListComponent, PupComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        HttpClientModule,
+        ExamplesRoutingModule,
+
+        // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+        // and returns simulated server responses.
+        // Remove it when a real server is ready to receive requests.
+        HttpClientInMemoryWebApiModule.forRoot(
+            InMemoryDataService, { dataEncapsulation: false }
+        )
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
     providers: [],
-    bootstrap: [ AppComponent ]
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
